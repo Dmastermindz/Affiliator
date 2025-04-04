1130bef4a46a8bf0164a41324e0033e9d1274695
“kirstenpomales” <kirstenpomales@gmail.com>
2025-04-04 17:43:09 +0300

    fixinf colors?

--- src/components/cta-section.tsx
+++ src/components/cta-section.tsx
@@ -6,7 +6,7 @@ export function CtaSection() {
   return (
     <section className="container flex flex-col items-center gap-6 py-24 sm:gap-10">
       <h2 className="max-w-xl text-balance text-center font-heading text-3xl font-semibold tracking-tight sm:text-4xl sm:leading-tight">
-        Code at the speed of no-code
+        Do awesome things
       </h2>
       <p className="max-w-lg text-balance text-center text-lg text-muted-foreground">
         Build at the speed of no-code. Export to Next.js and Tailwind code. Customize without
